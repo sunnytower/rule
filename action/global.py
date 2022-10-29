b0785f8c4f2d5aea865d0b02a3eb9a4a3341f371
jesse <77821870+sunnytower@users.noreply.github.com>
2022-10-29 16:47:34 +0800

    Create global.py

---
+++ action/global.py
@@ -0,0 +1,17 @@
+import requests
+Global = requests.get("https://ruleset.skk.moe/List/non_ip/global.conf").text
+Global_plus = requests.get("https://ruleset.skk.moe/List/non_ip/global_plus.conf").text
+#Blocked = requests.get("https://raw.githubusercontent.com/Blankwonder/surge-list/master/blocked.list").text
+
+result = list()
+result.extend(["DOMAIN-SUFFIX,jable.tv", "DOMAIN-SUFFIX,x18r.com", "DOMAIN-SUFFIX,av01.tv", "DOMAIN-SUFFIX,18comic.vip", "DOMAIN-KEYWORD,jav"])
+for rawresult in [Global, Global_plus, Blocked]:
+    for item in rawresult.split("\n"):
+        if (item not in result) and (not item.startswith('#')) :
+            result.append(item)
+
+result_text = '\n'.join(result)
+
+
+with open("./proxy.list", "w") as f:
+    f.write(result_text)
