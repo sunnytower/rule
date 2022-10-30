0069e726f52fdb51cf125e144bcc7c56fc7c2370
jesse <77821870+sunnytower@users.noreply.github.com>
2022-10-30 10:25:55 +0800

    Create script.sh

---
+++ action/script.sh
@@ -0,0 +1,8 @@
+
+git config --local user.email "ljxreal1@gmail.com"
+git config --local user.name "Sunnytower"
+git status | grep "clean"
+  if [[ $? -ne 0 ]]; then
+  git add ./*
+  git commit -m "update rules" -a
+  fi
