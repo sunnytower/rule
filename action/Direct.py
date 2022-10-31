e27b24ae5a1ab9cac76cd589a0d51836ff394b89
jesse <77821870+sunnytower@users.noreply.github.com>
2022-10-31 18:17:53 +0800

    Update Direct.py

--- action/Direct.py
+++ action/Direct.py
@@ -1,9 +1,9 @@
 import requests
 Scholar = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Scholar/Scholar.list").text
 Direct = requests.get("https://ruleset.skk.moe/List/non_ip/domestic.conf").text
-Blocked = requests.get("https://raw.githubusercontent.com/Blankwonder/surge-list/master/blocked.list").text
+
 result = list()
-for rawresult in [Scholar, Direct, Blocked]:
+for rawresult in [Scholar, Direct]:
     for item in rawresult.split("\n"):
         if (item not in result) and (not item.startswith('#')) :
             result.append(item)
