150463febaa6587e0cd76fdf6ea8eed8c1b96631
jesse <77821870+sunnytower@users.noreply.github.com>
2022-10-31 18:47:24 +0800

    Update Global.py

--- action/Global.py
+++ action/Global.py
@@ -4,9 +4,10 @@ Global_plus = requests.get("https://ruleset.skk.moe/List/non_ip/global_plus.conf
 Telegram0 = requests.get("https://ruleset.skk.moe/List/non_ip/telegram.conf").text
 Telegram1 = requests.get("https://ruleset.skk.moe/List/ip/telegram.conf").text
 Blocked = requests.get("https://raw.githubusercontent.com/Blankwonder/surge-list/master/blocked.list").text
-
+Spotify = requests.get("https://raw.githubusercontent.com/DivineEngine/Profiles/master/Surge/Ruleset/StreamingMedia/Music/Spotify.list").text
+Youtube = requests.get("https://raw.githubusercontent.com/DivineEngine/Profiles/master/Surge/Ruleset/StreamingMedia/Video/YouTube.list").text
 result = list()
-for rawresult in [Global, Global_plus, Telegram0, Telegram1, Blocked]:
+for rawresult in [Global, Global_plus, Telegram0, Telegram1, Blocked, Spotify, Youtube]:
     for item in rawresult.split("\n"):
         if (item not in result) and (not item.startswith('#')) :
             result.append(item)
