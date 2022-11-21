8e3034daf0a01f395202555ba3a061961707bafb
jesse <77821870+sunnytower@users.noreply.github.com>
2022-11-21 13:42:04 +0800

    Update BlackMatrix7_GlobalForClash.py

--- action/BlackMatrix7_GlobalForClash.py
+++ action/BlackMatrix7_GlobalForClash.py
@@ -7,5 +7,5 @@ for rawresult in [Global]:
     result.extend([item for item in rawresult.split("\n") if not item.startswith('#')])
 result_text = '\n'.join(result)
 
-with open("./Surge/BM/Clash/Global.list", "w") as f:
+with open("./Surge/BM/GlobalForClash.list", "w") as f:
     f.write("\n".join(result))
