743045dec3227eb5c5fe527ea7e05367f0609c83
jesse <77821870+sunnytower@users.noreply.github.com>
2023-04-09 17:07:04 +0800

    update microsoft rules

--- action/Microsoft.py
+++ action/Microsoft.py
@@ -1,11 +1,11 @@
 import requests
 
-Microsoft = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Microsoft/Microsoft.list").text
+Microsoft = requests.get("https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Microsoft.list").text
 
 result = list()
 for rawresult in [Microsoft]:
     result.extend([item for item in rawresult.split("\n") if not item.startswith('#')])
 result_text = '\n'.join(result)
 
-with open("./Surge/BM/Microsoft.list", "w") as f:
+with open("./Surge/Microsoft.list", "w") as f:
     f.write("\n".join(result))
