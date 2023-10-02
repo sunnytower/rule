cd2554e9c8ab91ea22f25ec6df346e25b2a07156
jesse <77821870+sunnytower@users.noreply.github.com>
2023-10-02 07:16:52 -0500

    update rules

--- Action/surge/Direct.py
+++ Action/surge/Direct.py
@@ -3,10 +3,9 @@ Scholar = requests.get("https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master
 Domestic = requests.get("https://ruleset.skk.moe/List/non_ip/domestic.conf").text
 # contains download.
 Direct = requests.get("https://ruleset.skk.moe/List/non_ip/direct.conf").text
-CN = requests.get("https://raw.githubusercontent.com/Blankwonder/surge-list/master/cn.list").text
 
 result = list()
-for rawresult in [Scholar, Domestic, CN]:
+for rawresult in [Scholar, Domestic]:
     for item in rawresult.split("\n"):
         if (item not in result) and (not item.startswith('#')) :
             result.append(item)
