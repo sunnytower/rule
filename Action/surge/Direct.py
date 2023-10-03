21221cc7069c8ef00ff8adab9f0d32cde4638d72
jesse <77821870+sunnytower@users.noreply.github.com>
2023-10-02 21:42:26 -0500

    upgrade rules.

--- Action/surge/Direct.py
+++ Action/surge/Direct.py
@@ -3,9 +3,10 @@ Scholar = requests.get("https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master
 Domestic = requests.get("https://ruleset.skk.moe/List/non_ip/domestic.conf").text
 # contains download.
 Direct = requests.get("https://ruleset.skk.moe/List/non_ip/direct.conf").text
-
+LAN = requests.get("https://github.com/blackmatrix7/ios_rule_script/blob/master/rule/Surge/Lan/Lan.list").text
+DirectIP = requests.get("https://ruleset.skk.moe/List/ip/domestic.conf").text
 result = list()
-for rawresult in [Scholar, Domestic]:
+for rawresult in [Scholar, Domestic, Direct, LAN, DirectIP]:
     for item in rawresult.split("\n"):
         if (item not in result) and (not item.startswith('#')) :
             result.append(item)
---
+++ Action/surge/Proxy.py
@@ -0,0 +1,14 @@
+import requests
+cdn = requests.get("https://ruleset.skk.moe/List/non_ip/cdn.conf").text
+stream = requests.get("https://ruleset.skk.moe/List/non_ip/stream.conf").text
+streamIP = requests.get("https://ruleset.skk.moe/List/ip/stream.conf").text
+telegram = requests.get("https://ruleset.skk.moe/List/non_ip/telegram.conf")
+telegramIP = requests.get("https://ruleset.skk.moe/List/ip/telegram.conf").text
+
+result = list()
+for rawresult in [cdn, stream, streamIP, telegram, telegramIP]:
+    result.extend([item for item in rawresult.split("\n") if not item.startswith('#')])
+result_text = '\n'.join(result)
+
+with open("./Surge/Proxy.list", "w") as f:
+    f.write("\n".join(result))
