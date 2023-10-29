0ffccc5e1164fbde35d670d2beb5c2b3b73ca690
jesse <77821870+sunnytower@users.noreply.github.com>
2023-10-30 00:52:39 +0800

    Update Proxy.py

--- Action/surge/Proxy.py
+++ Action/surge/Proxy.py
@@ -4,9 +4,10 @@ stream = requests.get("https://ruleset.skk.moe/List/non_ip/stream.conf").text
 streamIP = requests.get("https://ruleset.skk.moe/List/ip/stream.conf").text
 telegram = requests.get("https://ruleset.skk.moe/List/non_ip/telegram.conf").text
 telegramIP = requests.get("https://ruleset.skk.moe/List/ip/telegram.conf").text
+paypal = requests.get("https://raw.githubusercontent.com/DivineEngine/Profiles/master/Surge/Ruleset/Extra/PayPal.list").text
 
 result = list()
-for rawresult in [cdn, stream, streamIP, telegram, telegramIP]:
+for rawresult in [cdn, stream, streamIP, telegram, telegramIP, paypal]:
     result.extend([item for item in rawresult.split("\n") if not item.startswith('#')])
 result_text = '\n'.join(result)
 
