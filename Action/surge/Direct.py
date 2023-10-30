74b79b1eff081c0647f3860832c8cbc1ffcfb095
jesse <77821870+sunnytower@users.noreply.github.com>
2023-10-30 09:19:16 +0800

    Update Direct.py

--- Action/surge/Direct.py
+++ Action/surge/Direct.py
@@ -2,10 +2,12 @@ import requests
 Scholar = requests.get("https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Ruleset/Scholar.list").text
 # Domestic = requests.get("https://ruleset.skk.moe/List/non_ip/domestic.conf").text
 # contains download.
-Direct = requests.get("https://ruleset.skk.moe/List/non_ip/direct.conf").text
-DirectIP = requests.get("https://ruleset.skk.moe/List/ip/domestic.conf").text
+#Direct = requests.get("https://ruleset.skk.moe/List/non_ip/direct.conf").text
+#DirectIP = requests.get("https://ruleset.skk.moe/List/ip/domestic.conf").text
+CN = requests.get("https://raw.githubusercontent.com/Blankwonder/surge-list/master/cn.list").text
+
 result = list()
-for rawresult in [Scholar, Direct, DirectIP]:
+for rawresult in [Scholar, CN]:
     for item in rawresult.split("\n"):
         if (item not in result) and (not item.startswith('#')) :
             result.append(item)
