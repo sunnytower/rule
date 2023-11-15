61ce7d69d775bc09fb35f9f29b76a03b8662ea16
jesse <77821870+sunnytower@users.noreply.github.com>
2023-11-15 21:20:47 +0800

    update clash game rules

--- Action/clash/Direct.py
+++ Action/clash/Direct.py
@@ -3,9 +3,11 @@ Scholar = requests.get("https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master
 # Domestic = requests.get("https://ruleset.skk.moe/Clash/non_ip/domestic.txt").text
 Direct = requests.get("https://ruleset.skk.moe/Clash/non_ip/direct.txt").text
 DirectIP = requests.get("https://ruleset.skk.moe/Clash/ip/domestic.txt").text
+# GameDownload = requests.get("https://raw.githubusercontent.com/LoveMyself666/ACL4SSR/master/Clash/GameDownload.list").text
+GameDownload = requests.get("https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Ruleset/GameDownload.list").text
 
 result = list()
-for rawresult in [Scholar, Direct, DirectIP]:
+for rawresult in [Scholar, Direct, DirectIP, GameDownload]:
     for item in rawresult.split("\n"):
         if (item not in result) and (not item.startswith('#')) :
             result.append(item)
--- Action/clash/Global.py
+++ Action/clash/Global.py
@@ -1,6 +1,7 @@
 import requests
 
-Global = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Proxy/Proxy.yaml").text
+Global = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Proxy/Proxy.list").text
+# GamePlatform = requests.get("https://raw.githubusercontent.com/LoveMyself666/ACL4SSR/master/Clash/GamePlatform.list")
 
 result = list()
 for rawresult in [Global]:
