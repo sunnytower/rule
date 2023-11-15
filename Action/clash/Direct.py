7846d8058cdd04b71d768c20b3c364d500a97e18
jesse <77821870+sunnytower@users.noreply.github.com>
2023-11-15 22:53:56 +0800

    remove clash game rules

--- Action/clash/Direct.py
+++ Action/clash/Direct.py
@@ -3,10 +3,10 @@ Scholar = requests.get("https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master
 Domestic = requests.get("https://ruleset.skk.moe/Clash/non_ip/domestic.txt").text
 Direct = requests.get("https://ruleset.skk.moe/Clash/non_ip/direct.txt").text
 DirectIP = requests.get("https://ruleset.skk.moe/Clash/ip/domestic.txt").text
-Game = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Game/Game.list").text
+# Game = requests.get("https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Game/Game.list").text
 
 result = list()
-for rawresult in [Scholar, Domestic, Direct, DirectIP, Game]:
+for rawresult in [Scholar, Domestic, Direct, DirectIP]:
     for item in rawresult.split("\n"):
         if (item not in result) and (not item.startswith('#')) :
             result.append(item)
