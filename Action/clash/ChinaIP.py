ab3eb1f82aa7c1a4f2b842619870b3e468e27cc4
jesse <77821870+sunnytower@users.noreply.github.com>
2023-11-20 22:44:38 +0800

    update chinaIP rules

---
+++ Action/clash/ChinaIP.py
@@ -0,0 +1,14 @@
+import requests
+urls = [
+https://ruleset.skk.moe/Clash/ip/china_ip.txt,
+]
+result = []
+for url in urls:
+    resource_text = requests.get(url).text
+    for item in resource_text.split("\n"):
+        if (item not in result) and (not item.startswith('#') and (not item.startswith("USER-AGENT"))):
+            result.append(item)
+
+
+with open("./Clash/ChinaIP.txt", "w") as f:
+    f.write("\n".join(result))
