c64c56be8d44923f631c12f51e170507e49d1ac3
jesse <77821870+sunnytower@users.noreply.github.com>
2023-11-21 10:17:00 +0800

    updatechinaIP rules

--- Action/clash/ChinaIP.py
+++ Action/clash/ChinaIP.py
@@ -6,7 +6,7 @@ result = []
 for url in urls:
     resource_text = requests.get(url).text
     for item in resource_text.split("\n"):
-        if (item not in result) and (not item.startswith('#') and (not item.startswith("USER-AGENT"))):
+        if (item not in result) and (not item.startswith('#')):
             result.append(item)
 
 
