05d41b31b4abd241ff08dba674465312d3d6f240
jesse <77821870+sunnytower@users.noreply.github.com>
2023-11-25 22:02:31 +0800

    update openAI

---
+++ action/surge/OpenAI.py
@@ -0,0 +1,14 @@
+import requests
+urls = [
+"https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Ruleset/OpenAi.list",
+]
+result = []
+for url in urls:
+    resource_text = requests.get(url).text
+    for item in resource_text.split("\n"):
+        if (item not in result) and (not item.startswith('#')):
+            result.append(item)
+
+
+with open("./surge/OpenAI.list", "w") as f:
+    f.write("\n".join(result))
