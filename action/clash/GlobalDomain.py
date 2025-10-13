6bb48bfe4c731c29fbf21ed5d6e1f9412b48518e
jesse <77821870+sunnytower@users.noreply.github.com>
2025-10-14 01:34:57 +0800

    bug fiexed

--- action/clash/GlobalDomain.py
+++ action/clash/GlobalDomain.py
@@ -10,5 +10,5 @@ for url in urls:
             result.append(item)
 
 
-with open("./clash/GlobalDomain.list", "w") as f:
+with open("./clash/GlobalDomain.txt", "w") as f:
     f.write("\n".join(result))
